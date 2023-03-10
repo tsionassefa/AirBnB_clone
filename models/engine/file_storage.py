43972673f018ecaf09f033951a7b1dd9025bf631
tsionassefa <61678984+tsionassefa@users.noreply.github.com>
2023-03-10 22:20:58 +0300

    done

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,6 +5,12 @@ file to instances:
 
 import json
 from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 
