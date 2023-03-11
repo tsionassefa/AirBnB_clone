67472e4ca1aeca33b15caef93e68f87f3396374a
tsionassefa <61678984+tsionassefa@users.noreply.github.com>
2023-03-11 15:21:18 +0300

    save

--- models/base_model.py
+++ models/base_model.py
@@ -1,43 +1,55 @@
 #!/usr/bin/python3
-"""Model Base """
+"""This script is the base model"""
+
 import uuid
-import models
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
-    """class Base"""
+
+    """Class from which all other classes will inherit"""
+
     def __init__(self, *args, **kwargs):
-         """ Constructor """
-         if kwargs:
-            for key, value in kwargs.items():
+        """Initializes instance attributes
+        Args:
+            - *args: list of arguments
+            - **kwargs: dict of key-values arguments
+        """
+
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
                 if key == "created_at":
-                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                if key == "updated_at":
-                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                if key != "__class__":
-                    setattr(self, key, value)
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
         else:
-            self.id = str(uuid.uuid4())  # unique id
-            self.created_at = datetime.now()  # datetime when is created
-            self.updated_at = datetime.now()  # date when is updated
-            models.storage.new(self)
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
-def __str__(self):
-        """ print() __str__ method """
-        """" For pep8 validation"""
-        className = self.__class__.__name__
-        return "[{}] ({}) {}".format(className, self.id, self.__dict__)
+    def __str__(self):
+        """Returns official string representation"""
 
-def save(self):
-        """ updates with the current datetime """
+        return "[{}] ({}) {}".\
+            format(type(self).__name__, self.id, self.__dict__)
+
+    def save(self):
+        """updates the public instance attribute updated_at"""
+
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
-def to_dict(self):
-        '''returns a dictionary with all keys/value of the instance'''
-        dict_copy = self.__dict__.copy()
-        dict_copy["created_at"] = self.created_at.isoformat()
-        dict_copy["updated_at"] = self.updated_at.isoformat()
-        dict_copy['__class__'] = self.__class__.__name__
-        return dict_cop
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__"""
+
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = type(self).__name__
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        return my_dict
